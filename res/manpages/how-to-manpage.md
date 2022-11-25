ecb0a3a99b8cda0392a0886a50d0565ca530adc7
Developer Ramen <ramendev2009@gmail.com>
2022-11-25 20:13:06 +0700

    FORMAT: Prettier reformat

--- res/manpages/how-to-manpage.md
+++ res/manpages/how-to-manpage.md
@@ -1,5 +1,6 @@
 We get it, our manpage system is so unnecessary and hard to read.
 
-Suck it, here's the [full guide](https://github.com/out-post/sentinel/blob/main/res/manpages/how-to-manpage.md) to reading those.
+Suck it, here's the [full guide](https://github.com/out-post/sentinel/blob/main/res/manpages/how-to-manpage.md) to
+reading those.
 
 Have a good fricking day.
--- src/commands/info/how-to-manpage.ts
+++ src/commands/info/how-to-manpage.ts
@@ -14,21 +14,21 @@ export class HowToManpage {
 	 */
 	@Slash({
 		name: "how-to-manpage",
-		description: "Displays information on how to read, write and make sense of manpages."
+		description: "Displays information on how to read, write and make sense of manpages.",
 	})
 	async howToManpage(
 		@SlashOption({
 			name: "broadcast",
 			description: "Whether to broadcast the output to the current channel",
 			type: ApplicationCommandOptionType.Boolean,
-			required: false
+			required: false,
 		})
 		broadcast = false,
 		interaction: CommandInteraction
 	): Promise<void> {
 		await interaction.reply({
 			ephemeral: !broadcast,
-			content: readFileSync("res/manpages/how-to-manpage.md", "utf-8")
+			content: readFileSync("res/manpages/how-to-manpage.md", "utf-8"),
 		});
 	}
 }
