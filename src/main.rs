b228309b319b385752e21ed5efe27e6cf319a5ca
Developer Ramen <ramendev2009@gmail.com>
2023-01-07 17:24:36 +0700

    FEATURE: Boilerplate code for Serenity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use serenity::{async_trait, Client};
+use serenity::client::{Context, EventHandler};
+use serenity::model::gateway::Ready;
+use serenity::prelude::GatewayIntents;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} has reached the Out-Post!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().expect("Failed to read .env file");
+    let token = env::var("DISCORD_BOT_TOKEN").expect("No Discord bot token found in the environment");
+    let intents = GatewayIntents::all();
+    let mut client =
+        Client::builder(&token, intents)
+            .event_handler(Handler)
+            .await
+            .expect("Something went wrong while creating the client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
